b40f7d591a4efe622e4952f6b4f38b700bf7621c
bc5g15 <bc5g15@soton.ac.uk>
2016-10-05 11:29:44 +0100

    Update Main.java
    Starting to add the anagram functionality

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-package com.company;
+package entry;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -9,8 +9,8 @@ import java.util.Scanner;
 
 
 
-public class Main {
-
+public class Start {
+	static String username = "";
     static String name = "";
     static String job = "";
     static String email = "";
@@ -23,7 +23,7 @@ public class Main {
         try {
             Document doc = Jsoup.connect(url+nameid).get();
 
-
+            username = nameid;
             //store all the data in an array, so I can check for blanks.
             String data[] = new String[4];
 
@@ -32,17 +32,20 @@ public class Main {
             phone = doc.select("[property$=telephone").text().split(" ")[0];
             email = doc.select("[property$=email]").text().split(" ")[0];
 
+            //If we can't find the name, we're screwed.
             if(name.equals(""))
             {
                 System.out.println("Sorry! Couldn't find anything");
             }
             else {
+            	//If we are here, everything is good!
                 System.out.println("Got the data!");
             }
 
         } catch(Exception e) {
+        	//Lots of things can go wrong. I can't even begin to think about the individual exception types.
             System.out.println("Something went wrong!");
-            System.out.print(e);
+            System.out.println(e);
         }
     }
 
@@ -54,17 +57,84 @@ public class Main {
             System.out.printf("Phone No:\t%s\n", phone);
         System.out.printf("Email:\t\t%s\n\n", email);
     }
+    
+    
+    public static void anagram()
+    {
+    	if(username.equals(""))
+    	{
+    		System.out.println("Please gather a user's data first!");
+    	}
+    	else
+    	{
+    		String url = "http://wordsmith.org/anagram/anagram.cgi?anagram=";
+    		//Sanitise the name for URL use.
+    		String output = name.replace(' ', '+');
+    		
+    		try{
+    			Document doc = Jsoup.connect(url+output).get();
+    			String fullSite = doc.html();
+    			
+    			int index = fullSite.indexOf("Displaying All");
+    			
+    		}catch(Exception e)
+    		{
+    			System.out.println("Something went wrong!");
+    			System.out.println(e);
+    		}
+    	}
+    }
+        
+    public static void userMenu()
+    {
+    	boolean finished = false;
+	    	while(!finished)
+	    	{
+	    	System.out.println("[1] Gather data for a given username");
+	    	System.out.println("[2] Print Name, Job Description, Phone number (if found) and email address");
+	    	System.out.println("[3] (IN PROGRESS) 50 Name anagrams!");
+	    	System.out.println("[0] Quit the program");
+	    	
+	    	Scanner in = new Scanner(System.in);
+	    	int choice = in.nextInt();
+	    	
+	    	switch(choice)
+	    	{
+	    	case 1:
+	    		System.out.printf("Enter an ECS username:\n");
+	    		//This just means we don't pick up any junk;
+	    		in.nextLine();
+	    		String ecsName = in.nextLine();
+	    		getData(ecsName);
+	    		break;
+	    	case 2:
+	    		printData();
+	    		break;
+	    	case 0:
+	    		finished = true;
+	    		System.out.println("Goodbye!");
+	    		in.close();
+	    		break;
+	    	default:
+	    		System.out.println("What? Didn't understand that");
+	    	}
+    	}
+    }
 
 
 
     public static void main(String[] args) {
 	// write your code here
-        Scanner in = new Scanner(System.in);
-        getData("sjt4g11");
-        getData("rfp");
-        printData();
-
+//        Scanner in = new Scanner(System.in);
+//        getData("sjt4g11");
+//        getData("rfp");
+//        printData();  
+      
         //TODO: Add user input loop for easy use.
+        System.out.println("Welcome to the ECS Detail finder!");
+        System.out.println("Please enter one of the following commands:");
+        userMenu();
+        
 
        // System.out.print("hello");
     }
