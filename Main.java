a49129193786334b71fd12cd719c986fa4eeb707
bc5g15 <bc5g15@soton.ac.uk>
2016-10-05 16:32:34 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-package entry;
+package com.company;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -36,12 +36,12 @@ public class WebScraper {
                 System.out.println("Sorry! Couldn't find anything");
             }
             else {
-            	//If we are here, everything is good!
+                //If we are here, everything is good!
                 System.out.println("Got the data!");
             }
 
         } catch(Exception e) {
-        	//Lots of things can go wrong. I can't even begin to think about the individual exception types.
+            //Lots of things can go wrong. I can't even begin to think about the individual exception types.
             System.out.println("Something went wrong!");
             System.out.println(e);
         }
@@ -55,94 +55,106 @@ public class WebScraper {
             System.out.printf("Phone No:\t%s\n", phone);
         System.out.printf("Email:\t\t%s\n\n", email);
     }
-    
-    
+
+
     public static void anagram()
     {
-    	if(username.equals(""))
-    	{
-    		System.out.println("Please gather a user's data first!");
-    	}
-    	else
-    	{
-    		String url = "http://wordsmith.org/anagram/anagram.cgi?anagram=";
-    		//Sanitise the name for URL use.
-    		String output = name.replace(' ', '+');
-    		
-    		try{
-    			Document doc = Jsoup.connect(url+output).get();
-    			String fullSite = doc.html();
-    			
-    			int index = fullSite.indexOf("document.body.style.cursor='wait';");
-    			String shortSite = fullSite.substring(index);
-    			String names[] = shortSite.split("<br>");
-    			
-    			// for some reason formatting is out on the first name in the list
-    			names[1] = " " + names[1];
-    			
-    			for(int i = 1; i<51; i++)
-    			{
-    				System.out.printf("%d. \t%s\n", i, names[i]);
-    			}
-    			
-    		}catch(Exception e)
-    		{
-    			System.out.println("Something went wrong!");
-    			System.out.println(e);
-    		}
-    	}
+        if(username.equals(""))
+        {
+            System.out.println("Please gather a user's data first!");
+        }
+        else
+        {
+            String url = "http://wordsmith.org/anagram/anagram.cgi?anagram=";
+            //Sanitise the name for URL use.
+            String output = name.replace(' ', '+');
+
+            try{
+                Document doc = Jsoup.connect(url+output).get();
+                String fullSite = doc.html();
+
+                int index = fullSite.indexOf("document.body.style.cursor='wait';");
+                String shortSite = fullSite.substring(index);
+                String names[] = shortSite.split("<br>");
+
+                // for some reason formatting is out on the first name in the list
+                names[1] = " " + names[1];
+
+                for(int i = 1; i<51; i++)
+                {
+                    System.out.printf("%d.\t%10s\n", i, names[i]);
+                }
+
+            }catch(Exception e)
+            {
+                System.out.println("Something went wrong!");
+                System.out.println(e);
+            }
+        }
     }
-        
+
     public static void userMenu()
     {
-    	boolean finished = false;
-	    	while(!finished)
-	    	{
-	    	System.out.println("[1] Gather data for a given username");
-	    	System.out.println("[2] Print Name, Job Description, Phone number (if found) and email address");
-	    	System.out.println("[3] (IN PROGRESS) 50 Name anagrams!");
-	    	System.out.println("[0] Quit the program");
-	    	
-	    	Scanner in = new Scanner(System.in);
-	    	int choice = in.nextInt();
-	    	System.out.println();
-	    	
-	    	switch(choice)
-	    	{
-	    	case 1:
-	    		System.out.printf("Enter an ECS username:\n");
-	    		//This just means we don't pick up any junk;
-	    		in.nextLine();
-	    		String ecsName = in.nextLine();
-	    		getData(ecsName);
-	    		break;
-	    	case 2:
-	    		printData();
-	    		break;
-	    	case 3:
-	    		anagram();
-	    		break;
-	    	case 0:
-	    		finished = true;
-	    		System.out.println("Goodbye!");
-	    		in.close();
-	    		break;
-	    	default:
-	    		System.out.println("What? Didn't understand that");
-	    	}
-    	}
+        boolean finished = false;
+        while(!finished)
+        {
+            System.out.println("[1] Gather data for a given username");
+            System.out.println("[2] Print Name, Job Description, Phone number (if found) and email address");
+            System.out.println("[3] 50 Name anagrams!");
+            System.out.println("[0] Quit the program");
+
+            Scanner in = new Scanner(System.in);
+            int choice = Integer.MAX_VALUE;
+            while(choice == Integer.MAX_VALUE)
+            {
+                if(!in.hasNextInt())
+                {
+                    System.out.println("Please enter a number!");
+                    in.nextLine();
+                }
+                else
+                {
+                    choice = in.nextInt();
+                }
+            }
+            System.out.println();
+
+            switch(choice)
+            {
+                case 1:
+                    System.out.printf("Enter an ECS username:\n");
+                    //This just means we don't pick up any junk;
+                    in.nextLine();
+                    String ecsName = in.nextLine();
+                    getData(ecsName);
+                    break;
+                case 2:
+                    printData();
+                    break;
+                case 3:
+                    anagram();
+                    break;
+                case 0:
+                    finished = true;
+                    System.out.println("Goodbye!");
+                    in.close();
+                    break;
+                default:
+                    System.out.println("What? Didn't understand that");
+            }
+        }
     }
 
 
 
     public static void main(String[] args) {
-      
+
         //TODO: Add user input loop for easy use.
         System.out.println("Welcome to the ECS Detail finder!");
         System.out.println("Please enter one of the following commands:");
         userMenu();
-        
 
-       // System.out.print("hello");
+
+        // System.out.print("hello");
     }
 }
